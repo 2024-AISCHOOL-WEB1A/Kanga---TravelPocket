76b608ef7e22ed12859a317e64189f131ded45fd
표동우 <god11dog@naver.com>
2024-07-23 10:51:24 +0900

    외부 스크립트 문제 해결

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const path = require('path') // 경로를 수정하는 모듈
 const file_path = path.join(__dirname, "../../Kanga---TravelPocket")
 
 
-
 app.use(express.static(file_path));
 app.use(express.static('../Kanga---TravelPocket')); // 'public' 디렉토리에서 정적 파일을 제공하는 예
+app.use(express.static('../Kanga---TravelPocket/main'));
+app.use(express.static('../Kanga---TravelPocket/accounts'));
+app.use(express.static('../Kanga---TravelPocket/checklist'));
+app.use(express.static('../Kanga---TravelPocket/static'));
 
 app.use('/', express.static(file_path + '/'));
 
