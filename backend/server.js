54a603045dfa7fb4471d237fc1b6a36426307210
표동우 <god11dog@naver.com>
2024-07-24 16:22:42 +0900

    오류 수정

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,6 @@ const insertData = require('./insert'); // 데이터 삽입 모듈
 const bodyParser = require('body-parser');
 
 
-
-
-
 const app = express();
 const port = 3000;
 
@@ -30,42 +27,9 @@ app.use('/user', userRouter);
 app.use('/user/update', updateUserRouter);
 app.use('/user/delete', deleteUserRouter);
 
-// 메인 라우터 설정
+
 app.use('/', mainRouter);
 
 app.listen(port, () => {
     console.log(`서버가 포트 ${port}에서 실행 중입니다.`);
-});
-
-
-
-//---------------------------------------------------------------------------------------------------
-
-// app.use(express.static(file_path));
-app.use(express.static('../Kanga---TravelPocket')); // 'public' 디렉토리에서 정적 파일을 제공하는 예
-app.use(express.static('../Kanga---TravelPocket/main'));
-app.use(express.static('../Kanga---TravelPocket/accounts'));
-app.use(express.static('../Kanga---TravelPocket/checklist'));
-app.use(express.static('../Kanga---TravelPocket/static'));
-app.use(express.static('../Kanga---TravelPocket/intro'));
-// app.use('/', express.static(file_path + '/'));
-
-app.use('/', mainRouter)
-
-
-// ----------------------------- user_county에서 입력받은 국가 이름 전달 -----------------------------
-
-
-app.post('/save-country', (req, res) => {
-    const { country_idx, country_name } = req.body;
-
-    const query = 'INSERT INTO tb_travel_country (country_idx, country_name) VALUES (?)';
-    pool.query(query, [country_idx, country_name], (error, results) => {
-        if (error) {
-            console.error('데이터 삽입에 오류 발생 :', error.stack);
-            res.status(500).send('데이터베이스에 저장 실패');
-            return;
-        }
-        res.status(200).json({ message: '국가 정보 전달 성공' });
-    });
 });
