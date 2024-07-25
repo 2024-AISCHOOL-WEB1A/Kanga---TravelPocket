af76689c4cef5f80962f32fb7bd5df4405550850
LKH <rbsgh369@gmail.com>
2024-07-25 11:10:48 +0900

    크롤링 수정

--- backend/scheduler.js
+++ backend/scheduler.js
@@ -1,9 +1,9 @@
 const schedule = require('node-schedule');
 const crawlData = require('./crawling'); // 크롤링 모듈
-const insertData = require('./insert');   // 데이터 삽입 모듈
+const insertData = require('./insert'); // 데이터 삽입 모듈
 
-// 6시간마다 작업 실행 (0분에 실행)
-schedule.scheduleJob('0 */6 * * *', async () => {
+// 크롤링 및 데이터 삽입 함수 정의
+async function executeTasks() {
     console.log('크롤링 작업 시작');
     try {
         await crawlData();
@@ -13,4 +13,10 @@ schedule.scheduleJob('0 */6 * * *', async () => {
     } catch (error) {
         console.error(`작업 중 오류 발생: ${error.message}`);
     }
-});
+}
+
+// 즉시 한 번 실행
+executeTasks();
+
+// 주기적으로 크롤링하고 데이터베이스에 삽입하는 작업 예약
+schedule.scheduleJob('0 0 * * *', executeTasks);
