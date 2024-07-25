9989dcfbf21ec4d2f6a3ac7b770fc942dbf73a77
표동우 <>
2024-07-25 09:09:30 +0900

    header 수정

--- accounts/login.js
+++ accounts/login.js
@@ -1,26 +1,32 @@
+async function handleLogin(event) {
+    event.preventDefault(); // 폼 제출 방지
 
-document.getElementById('loginForm').addEventListener('submit', function(e) {
-    e.preventDefault();
+    const userId = document.getElementById('login_user_id').value;
+    const userPw = document.getElementById('login_user_pw').value;
 
-    const email = document.getElementById('loginEmail').value;
-    const password = document.getElementById('loginPassword').value;
+    try {
+        const response = await fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ user_id: userId, user_pw: userPw })
+        });
 
-    fetch('/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ user_id: email, user_pw: password })  
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.auth) {
-            alert('로그인 성공');
-            localStorage.setItem('token', data.token);
-        } else {
-            alert('로그인 실패');
+        const data = await response.json(); // JSON 형식으로 응답 받음
+
+        if (!response.ok) {
+            console.error('로그인 중 오류:', data.message);
+            alert('로그인 중 오류 발생: ' + data.message);
+            return;
         }
-    })
-    .catch(error => console.error('오류:', error));
-});
 
+        console.log('로그인 성공:', data.message);
+        alert('로그인 성공');
+        // 로그인 성공 후 리다이렉트 등 처리
+        window.location.href = 'index.html';
+    } catch (error) {
+        console.error('로그인 중 오류:', error.message);
+        alert('서버와의 연결 중 오류 발생');
+    }
+}
